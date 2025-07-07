82b0e7d9810e0392b4af1cbf5a65e49daee148b3
dufenz <arsinevic@gmail.com>
2025-07-07 16:02:53 +0300

    Completed 1.4 - refactor to multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,42 +1,33 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
-
-const Content = ({ part1, part2, part3 }) => {
-  return (
-    <div>
-      <p>{part1.name} — {part1.exercises} exercises</p>
-      <p>{part2.name} — {part2.exercises} exercises</p>
-      <p>{part3.name} — {part3.exercises} exercises</p>
-    </div>
-  )
-}
-
-const Total = ({ part1, part2, part3 }) => {
-  const total = part1.exercises + part2.exercises + part3.exercises
-  return <p>Total exercises: {total}</p>
-}
-
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <h1>{course}</h1>
+      {parts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+      <p>
+        <strong>
+          Total: {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
+        </strong>
+      </p>
     </div>
   )
 }
