08efec7b0e8d88062fe623522e4b42df226332f9
dufenz <arsinevic@gmail.com>
2025-07-07 10:07:13 +0300

    Completed 1.3 - use objects for parts

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,18 +1,42 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ part1, part2, part3 }) => {
+  return (
+    <div>
+      <p>{part1.name} — {part1.exercises} exercises</p>
+      <p>{part2.name} — {part2.exercises} exercises</p>
+      <p>{part3.name} — {part3.exercises} exercises</p>
+    </div>
+  )
+}
+
+const Total = ({ part1, part2, part3 }) => {
+  const total = part1.exercises + part2.exercises + part3.exercises
+  return <p>Total exercises: {total}</p>
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1} — {exercises1} exercises</p>
-      <p>{part2} — {exercises2} exercises</p>
-      <p>{part3} — {exercises3} exercises</p>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
