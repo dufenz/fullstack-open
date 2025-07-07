3a4569ef2cc857127064093791fd1e1d2ee4af6a
dufenz <arsinevic@gmail.com>
2025-07-07 20:28:34 +0300

    Completed 2.11 - add new note to server

---
+++ part2/2.11/App.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+import NoteList from './NoteList'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const toggleImportance = (id) => {
+    const updatedNotes = notes.map(note =>
+      note.id === id ? { ...note, important: !note.important } : note
+    )
+    setNotes(updatedNotes)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
+      <NoteList notes={notesToShow} toggleImportance={toggleImportance} />
+    </div>
+  )
+}
+
+export default App
