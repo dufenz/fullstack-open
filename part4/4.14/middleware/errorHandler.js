3a0d8e4e45b3844f51c6482624f1d885d60ed017
dufenz <arsinevic@gmail.com>
2025-07-08 11:37:36 +0300

    4.14: move config to utils/config.js

---
+++ part4/4.14/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+const logger = require('../utils/logger')
+
+module.exports = (error, req, res, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
