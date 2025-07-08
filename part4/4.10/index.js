921c556b2903694239e4e953b39afed60527a673
dufenz <arsinevic@gmail.com>
2025-07-08 11:31:07 +0300

    4.10: extracted routes to notesRouter module

---
+++ part4/4.10/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('connected to MongoDB'))
+  .catch(err => console.error('error connecting to MongoDB:', err.message))
+
+app.use('/api/notes', notesRouter)
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server running on port ${process.env.PORT || 3001}`)
+})
