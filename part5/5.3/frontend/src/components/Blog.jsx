0f9bac2f6c52bca28fdea9dee54359132ee1d511
dufenz <arsinevic@gmail.com>
2025-07-09 16:02:20 +0300

    5.15: move blog toggle logic to Blog component

--- part5/5.3/frontend/src/components/Blog.jsx
+++ part5/5.3/frontend/src/components/Blog.jsx
@@ -1,37 +1,24 @@
 import React, { useState } from 'react'
 
-const Blog = ({ blog, onLike, onRemove, currentUser }) => {
+const Blog = ({ blog, handleLike }) => {
   const [visible, setVisible] = useState(false)
 
   const toggleVisibility = () => setVisible(!visible)
 
-  const handleLike = () => {
-    onLike(blog)
-  }
-
-  const handleRemove = () => {
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
-      onRemove(blog)
-    }
-  }
-
   return (
-    <div className="blog" style={{ border: '1px solid black', padding: 5, marginBottom: 5 }}>
+    <div className="blog">
       <div>
         {blog.title} {blog.author}
         <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
       </div>
+
       {visible && (
-        <div className="blog-details">
+        <div className="blogDetails">
           <div>{blog.url}</div>
           <div>
-            likes {blog.likes}
-            <button onClick={handleLike}>like</button>
+            likes: {blog.likes}
+            <button onClick={() => handleLike(blog)}>like</button>
           </div>
-          <div>{blog.user?.name}</div>
-          {currentUser?.username === blog.user?.username && (
-            <button onClick={handleRemove}>remove</button>
-          )}
         </div>
       )}
     </div>
--- part5/5.3/frontend/src/components/BlogList.jsx
+++ part5/5.3/frontend/src/components/BlogList.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Blog from './Blog'
 
-const BlogList = ({ blogs }) => {
-  if (!blogs || !Array.isArray(blogs)) return null
-
+const BlogList = ({ blogs, handleLike }) => {
   return (
     <div>
       {blogs
         .slice()
         .sort((a, b) => b.likes - a.likes)
-        .map((blog) => (
-          <Blog key={blog.id} blog={blog} />
+        .map(blog => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleLike={handleLike}
+          />
         ))}
     </div>
   )
