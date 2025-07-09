b458e3f6a5d51bdfffcf486d72209698761ade27
dufenz <arsinevic@gmail.com>
2025-07-09 09:36:43 +0300

    Removed all folders in part5 except 5.1

--- part5/5.1/part2/2.10/src/services/notes.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = 'https://studies.cs.helsinki.fi/exampleapp/notes'
-
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
-
-export default { getAll }
